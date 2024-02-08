fb471bda1050ead1a722264655aba082ae13dd99
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-02-08 21:34:24 +0530

    update

---
+++ Expense_Tracker/main.js
@@ -0,0 +1,6 @@
+const balance=document.querySelector("#balance");
+const inc_amt=document.querySelector("#inc-amt");
+const exp_amt=document.querySelector("#exp-amt");
+const description=document.querySelector("#desc");
+const amount=document.querySelector("#amt");
+
